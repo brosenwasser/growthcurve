09d9a8307ac0edee14a155e2c351c562c9194045
brosenwasser <rosenwas@astro.wisc.edu>
2016-06-16 14:14:13 -0500

    Update gc.py

--- gc.py
+++ gc.py
@@ -1,5 +1,5 @@
   # what is this filter for?
-  ref_filter = np.loadtxt(target_dir + '/s_extraction/ref_filter.txt', unpack=True,  dtype='str')
+  #ref_filter = np.loadtxt(target_dir + '/s_extraction/ref_filter.txt', unpack=True,  dtype='str')
 
   # get image
   # imlist = glob.glob(target_dir + '/img/*_sci.fits')
@@ -11,17 +11,17 @@
   
   # Set the necessary variables for photometry on the reference image
     ref_exptime = pyfits.getheader(ref_image)['EXPTIME']
-    ref_inst = pyfits.getheader(ref_image)['INSTRUME']
-    ref_inst = ref_inst.lower()
+    #ref_inst = pyfits.getheader(ref_image)['INSTRUME']
+    #ref_inst = ref_inst.lower()
     
-    match = (inst_zp == ref_inst) & (filter_zp == ref_filter)
-    ref_zp = zp_zp[match]
+    #match = (inst_zp == ref_inst) & (filter_zp == ref_filter)
+    #ref_zp = zp_zp[match]
     
     
     # Define coordinate and output photometry file paths
     # the file number should be a variable for the full code
-    gals_coo = objects_040.coo
-    gals_mag = objects_040.mag
+    gals_coo = 'objects_040.coo'
+    gals_mag = 'objects_040.mag'
 
   # Set iraf parameters for reference filter 
     iraf.datapars.epadu = ref_exptime
@@ -32,7 +32,7 @@
     iraf.fitskypars.dannulus = 1. 
     
     iraf.photpars.apertures = '1.0,3.0,5.0,7.0,9.0,11.0,13.0,15.0,17.0,19.0,21.0,23.0,25.0,27.0,29.0,31.0,33.0,35.0,37.0,39.0'
-    iraf.photpars.zmag = ref_zp
+    #iraf.photpars.zmag = ref_zp
     
     
     # run phot for the image
